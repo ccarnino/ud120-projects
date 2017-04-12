164051ec795f7482bdce9fe5bba8dd8b65a294ad
Claudio Carnino <carninoclaudio@gmail.com>
2017-04-12 16:32:48 +0100

    Completed the naive bales accuracy exercise

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -25,9 +25,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+# Train the classifier
+classifier = GaussianNB()
+
+timeBeginTraining = time()
+classifier.fit(features_train, labels_train)
+timeEndTraining = time()
+
+print "Time required for training: ", round(timeEndTraining - timeBeginTraining, 3), "s"
+
+# Make predictions on the test set
+predictions = classifier.predict(features_test)
+
+# Get accuracy
+accuracy = accuracy_score(labels_test, predictions)
+print "Accuracty: ", accuracy
 
 #########################################################
-
-
