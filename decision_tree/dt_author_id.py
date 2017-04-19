e7cd4dfd3bdf686f022b6c590305223d76bd09a9
Claudio Carnino <carninoclaudio@gmail.com>
2017-04-19 16:12:59 +0100

    Implemented the Decision Tree classifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,11 +21,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import numpy as np
 
+# Train the classifier
+classifier = tree.DecisionTreeClassifier(min_samples_split=40)
+
+trainingTimeBegin = time()
+classifier.fit(features_train, labels_train)
+trainingTimeEnd = time()
+
+print "Time required for training: ", round(trainingTimeEnd - trainingTimeBegin, 3), "s"
+
+# Make predictions on the test set
+predictingTimeStart = time()
+predictions = classifier.predict(features_test)
+
+predictingTimeEnd = time()
+print "Time required for predicting: ", round(predictingTimeEnd - predictingTimeStart, 3), "s"
+
+# Get accuracy
+accuracy = accuracy_score(labels_test, predictions)
+print "Accuracty: ", accuracy
 
 #########################################################
-
-
