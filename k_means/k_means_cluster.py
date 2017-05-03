1ccf89709a182c616ac74285c1624d7c893f1451
Claudio Carnino <carninoclaudio@gmail.com>
2017-05-03 21:06:08 +0100

    Implemented the clustering exercise

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
     Skeleton code for k-means clustering mini-project.
 """
 
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from math import isnan
 
 
 
@@ -40,37 +41,81 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+# Get the min and max exercised_stock_options
+minExercisedStockOptions = sys.maxint
+maxExercisedStockOptions = -sys.maxint-1
+
+for personName in data_dict:
+    personExercisedStockOptions = data_dict[personName]["exercised_stock_options"]
+    if isnan(float(personExercisedStockOptions)):
+        continue
+
+    if personExercisedStockOptions > maxExercisedStockOptions:
+        maxExercisedStockOptions = personExercisedStockOptions
+
+    if personExercisedStockOptions < minExercisedStockOptions:
+        minExercisedStockOptions = personExercisedStockOptions
+
+print "Exercised stock options: min = ", minExercisedStockOptions, "; max = ", maxExercisedStockOptions
+
+
+
+# Get the min and max salary
+minSalary = sys.maxint
+maxSalary = -sys.maxint-1
+
+for personName in data_dict:
+    personSalary = data_dict[personName]["salary"]
+    if isnan(float(personSalary)):
+        continue
+
+    if personSalary > maxSalary:
+        maxSalary = personSalary
+
+    if personSalary < minSalary:
+        minSalary = personSalary
+
+print "Salary: min = ", minSalary, "; max = ", maxSalary
+
+
+
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
 
+kmeans = KMeans(n_clusters=2, random_state=0)
+kmeans.fit(finance_features)
+
+pred = kmeans.labels_
 
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_3features.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
