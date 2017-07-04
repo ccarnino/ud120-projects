900380814d37c46c12e738d59e2a8d23f98e8e4a
Claudio Carnino <carninoclaudio@gmail.com>
2017-07-04 16:19:39 +0100

    Completed lesson 14

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -27,6 +27,14 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward!
+from sklearn import tree
+from sklearn.model_selection import train_test_split
 
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(train_features, train_labels)
+
+score = classifier.score(test_features, test_labels)
+print "Score: ", score
